52412a4ad240988c599686f8ccc59285059d1a45
Jace Trimmer <blendidoperatorjt@gmail.com>
2026-03-17 07:44:21 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -77,6 +77,7 @@ import Settings from './pages/Settings';
 import Stretches from './pages/Stretches';
 import Terms from './pages/Terms';
 import Upload3DModels from './pages/Upload3DModels';
+import Stretches from './pages/Stretches';
 import WatchMode from './pages/WatchMode';
 import WorkoutBuilder from './pages/WorkoutBuilder';
 import WorkoutComplete from './pages/WorkoutComplete';
